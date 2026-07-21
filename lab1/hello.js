5558f9f7ea09dfc74edc2412d01a51cabc200607
Akshat Kumar <forakshatkumar@gmail.com>
2026-07-21 14:39:49 +0530

    Updated files

---
+++ lab1/hello.js
@@ -0,0 +1,8 @@
+function sayhello(name) {
+  console.log(`Welcome to ${name}`);
+}
+sayhello("Akshat Kumar");
+function sum(a, b) {
+  return a + b;
+}
+console.log(`sum of 2 and 5 is = ${sum(2, 5)}`);
