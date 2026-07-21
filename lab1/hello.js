408e1508e8039645f8c07a5c2f682fbcb3477c38
Akshat Kumar <forakshatkumar@gmail.com>
2026-07-21 14:47:55 +0530

    add function

--- lab1/hello.js
+++ lab1/hello.js
@@ -6,3 +6,18 @@ function sum(a, b) {
   return a + b;
 }
 console.log(`sum of 2 and 5 is = ${sum(2, 5)}`);
+
+//arrow head function
+const sayHi = (name) => {
+  console.log(`Welcome to ${name}`);
+};
+sayHi(`Akshat`);
+
+const add = (a, b) => {
+  console.log(`The addition is : `+ (a + b));
+};
+add(3, 4);
+
+//arrow head short
+const findsum = (a,b) => a+b;
+console.log(`sum of 2 and 5 is = ${findsum(2, 5)}`);
