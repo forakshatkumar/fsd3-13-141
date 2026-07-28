80d13b52789dc90f2cbdd609af3560e65b7b0c93
Akshat Kumar <forakshatkumar@gmail.com>
2026-07-28 14:39:49 +0530

    Updated files

--- lab1/eventloop.js
+++ lab1/eventloop.js
@@ -1,3 +1,5 @@
+import { writeFile } from "fs/promises";
+
 const f1 = () => {
   console.log("F1");
 };
@@ -8,11 +10,26 @@ const f3 = () => {
   console.log("F3");
 };
 
+const writeData = async () => {
+  await writeFile("Note.txt", "I am fs module");
+  console.log("File written");
+};
+
 const main = () => {
   console.log("main");
-  f1();
-  f2();
-  f3();
+  writeData();
+  // // f1();
+  setTimeout(f2, 0); //executes seprately after some time
+  // // setInterval(f2,1000); //executes repeatedly after given time
+  setImmediate(f3);
+  process.nextTick(f1);
   console.log("end");
+
+  new Promise((resolve, reject) => {
+    console.log("I am Promise 1");
+  });
+  new Promise((resolve, reject) => {
+    console.log("I am Promise 2");
+  });
 };
 main();
