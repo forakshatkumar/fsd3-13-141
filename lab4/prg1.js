122c020e5fac52bfc818d8e2a28b4c522f90fb9a
Akshat Kumar <forakshatkumar@gmail.com>
2026-09-23 12:20:27 +0530

    Express Starts

--- lab4/prg1.js
+++ lab4/prg1.js
@@ -3,9 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("<h1>Hello World</h1>");
 });
 
+//always listen at last
 app.listen(3333, () => {
   console.log("Server is running on 3333");
 });
