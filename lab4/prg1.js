18a815e2eb56e881c14f7dbd793d36c51e5be314
Akshat Kumar <forakshatkumar@gmail.com>
2026-09-23 12:14:58 +0530

    created prg 1

--- lab4/prg1.js
+++ lab4/prg1.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(3333, () => {
+  console.log("Server is running on 3333");
 });
